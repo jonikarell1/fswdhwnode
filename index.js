b38ec9c898bd38afb16d3bc52ae8ad17f3c174f0
unknown <jonikarell1@gmail.com>
2023-02-18 08:16:12 +0700

    inital commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const calc = require("./soal1")
+
+console.log(calc.squareArea(5))
+console.log(calc.squarePerimeter(5))
+
+console.log(calc.rectangleArea(5,4))
+console.log(calc.rectanglePerimeter(5,4))
---
+++ soal1.js
@@ -0,0 +1,15 @@
+exports.squareArea = function (side){
+    return side**2
+}
+
+exports.squarePerimeter = function (side){
+    return 4 * side
+}
+
+exports.rectangleArea = function (length, width){
+    return length * width
+}
+
+exports.rectanglePerimeter = function (length, width){
+    return 2 *(length+width)
+}
